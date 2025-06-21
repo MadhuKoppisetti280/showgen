20b526ae5d4ae873e9150863272478b1b4c906a6
Madhu9390848183 <madhukoppisetti280@gmail.com>
2025-06-21 09:41:23 +0530

    Replace repository with new files

--- pages/index.js
+++ pages/index.js
@@ -18,35 +18,36 @@ export default function Home() {
       <Head>
         <title>Showgen</title>
       </Head>
-    
+
       <div className="min-h-screen flex items-center justify-center">
         <Image
           className="dark:invert"
           src="/showgen.svg"
           alt="Showgen logo"
           width={180}
-          height={38}
+          height={98}
           priority
         />
       </div>
-      <br/>
+      <br />
       <div
         className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
       >
         <main className="min-h-screen flex flex-col items-center justify-center">
           <h1 className="text-3xl font-bold mt-4">Showgen</h1>
-          <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            <li className="mb-2 tracking-[-.01em]">
+          <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)] space-y-2 mt-4">
+            <li className="tracking-[-.01em]">
               Welcome to Showgen! This is your new portfolio site.
             </li>
             <li className="tracking-[-.01em]">
               Save and see your changes instantly.
             </li>
           </ol>
-          <br/>
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <br />
+          <div className="flex gap-4 items-center flex-col sm:flex-row mt-6">
+
             <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto no-underline"
               href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
@@ -58,15 +59,16 @@ export default function Home() {
                 width={20}
                 height={20}
               />
-    
+
               Deploy now
             </a>
-            <br/>
+            <br />
             <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
               href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
+              style={{ textDecoration: "none" }}
             >
               Read our docs
             </a>
@@ -76,8 +78,9 @@ export default function Home() {
           <span className="text-sm text-gray-500">
             © 2025 Showgen. Founded by Madhu Mahalakshmi.
           </span>
+
           <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            className="flex items-center gap-2 text-sm !text-gray-500 visited:!text-gray-500 no-underline hover:underline hover:underline-offset-4"
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -92,7 +95,7 @@ export default function Home() {
             Learn
           </a>
           <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            className="flex items-center gap-2 text-sm !text-gray-500 visited:!text-gray-500 no-underline hover:underline hover:underline-offset-4"
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -107,7 +110,7 @@ export default function Home() {
             Examples
           </a>
           <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            className="flex items-center gap-2 text-sm !text-gray-500 visited:!text-gray-500 no-underline hover:underline hover:underline-offset-4"
             href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -122,6 +125,7 @@ export default function Home() {
             Go to nextjs.org →
           </a>
         </footer>
+
       </div>
     </>
   );
